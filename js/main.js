82b19ae84eaab551c96509577842bfb553edc056
U256 <65955446+U256@users.noreply.github.com>
2020-08-16 18:52:10 +1000

    notfication-bar sizing & blocks switch fixes

--- js/main.js
+++ js/main.js
@@ -2,16 +2,22 @@
 
 let notificationsBell = document.querySelector('.notifications-btn');
 let notificationsX = document.querySelector('.close-bar__btn');
-
 let notificationsBar = document.querySelector('.notifications-bar');
 
 
 notificationsBell.addEventListener('click', e => {
-  notificationsBar.classList.toggle('notifications-bar__active')
+  let isNotifBarOn = notificationsBar.classList.toggle('notifications-bar__active')
+
+  if (isNotifBarOn) {
+    setTimeout(() => {
+      notificationsBar.classList.remove('notifications-bar__active')
+    }, 6000)
+  }
 })
 notificationsX.addEventListener('click', e => {
   notificationsBar.classList.remove('notifications-bar__active')
 })
+
 
 
 
@@ -22,34 +28,27 @@ document.querySelector('.left-panel__switcher').addEventListener('click', e => {
 
 
 
-// let dashboardToBlockBtn = document.querySelector('.dashboard-toblock-btn');
-// let usersToBlockBtn = document.querySelector('.users-toblock-btn');
-// let productsToBlockBtn = document.querySelector('.products-toblock-btn');
-// let authentifToBlockBtn = document.querySelector('.authentication-toblock-btn');
-// let typographyToBlockBtn = document.querySelector('.typography-toblock-btn');
-// let iconsImagesToBlockBtn = document.querySelector('.icons-images-toblock-btn');
-
-// let dashboardBlock = document.querySelector('.dashboard-block');
-// let usersBlock = document.querySelector('.users-block');
-// let productsBlock = document.querySelector('.products-block');
-// let authentifBlock = document.querySelector('.authentication-block');
-// let typographyBlock = document.querySelector('.typography-block');
-// let iconsImagesBlock = document.querySelector('.icons-images-block');
-
-// let [dashboardToBlockBtn, usersToBlockBtn, productsToBlockBtn, authentifToBlockBtn, typographyToBlockBtn, iconsImagesToBlockBtn] = document.querySelectorAll('.toblock-btn');
-
 let toBlockButtonsArray = document.querySelectorAll('.toblock-btn');;
 let blocksArray = document.querySelectorAll('.prim-blocks')
 
-console.log(toBlockButtonsArray[0].getAttribute("rel"));
-console.log(blocksArray[0]);
+function deactivateBlock(block) {
+  block.style.position = 'absolute';
+  block.style.visibility = 'hidden';
+  block.style.opacity = 0;
+}
+function activateBlock(block) {
+  block.style.position = 'relative';
+  block.style.visibility = 'visible';
+  block.style.opacity = 1;
+}
 
+//activ //////////////
+//////////// //////////
+/////////////
 
-
-
-
-blocksArray[0].style.display = 'flex'
-
+// activate dashboard block when page loading
+//activateBlock(blocksArray[0])
+blocksArray[0].classList.add("activ");
 
 for (let i = 0; i < blocksArray.length; i++) {
   toBlockButtonsArray[i].addEventListener('click', function (e) {
@@ -57,18 +56,20 @@ for (let i = 0; i < blocksArray.length; i++) {
     toBlockButtonsArray.forEach((toBlockBtn) => {
       toBlockBtn.classList.remove("toblock-btn_active");
     });
-    
+
     blocksArray.forEach((block) => {
-      block.style.display = 'none'
+      // deactivateBlock(block)
+      block.classList.remove("activ");
     });
-
-    blocksArray[i].style.display = 'flex'
+    
+    // activateBlock(blocksArray[i]);
     toBlockButtonsArray[i].classList.add("toblock-btn_active");
+    blocksArray[i].classList.add("activ");
 
-
+    document.querySelector('.left-panel').classList.remove('left-panel_active')
   })
-
 }
+
 
 
 ////////////////////////////
