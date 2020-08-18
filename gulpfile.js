d9e9e0fdf6ff1777507dce250e267c66e2ae57b9
U256 <65955446+U256@users.noreply.github.com>
2020-08-18 21:27:03 +1000

    start js fixink

--- gulpfile.js
+++ gulpfile.js
@@ -11,21 +11,21 @@ let path = {
 		html: project_folder + "/",
 		css: project_folder + "/css/",
 		js: project_folder + "/js/",
-		img: project_folder + "/img/",
+		img: project_folder + "/images/",
 		fonts: project_folder + "/fonts/",
 	},
 	src: {
 		html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
 		css: source_folder + "/scss/style.scss",
 		js: source_folder + "/js/script.js",
-		img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+		img: source_folder + "/images/**/*.{jpg,png,svg,gif,ico,webp}",
 		fonts: source_folder + "/fonts/*.ttf",
 	},
 	watch: {
 		html: source_folder + "/**/*.html",
 		css: source_folder + "/scss/**/*.scss",
 		js: source_folder + "/js/**/*.js",
-		img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}"
+		img: source_folder + "/images/**/*.{jpg,png,svg,gif,ico,webp}"
 	},
 	clean: "./" + project_folder + "/"
 }
