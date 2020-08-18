980422fbee23defea57ce3e0ef310724fa39a620
U256 <65955446+U256@users.noreply.github.com>
2020-08-18 15:26:50 +1000

    dashboard in work

--- js/enter-page.js
+++ js/enter-page.js
@@ -11,40 +11,36 @@ let startStep = document.querySelector('.enter-start');
 let registrationStep = document.querySelector('.enter-registration');
 let loginStep = document.querySelector('.enter-login');
 
-let toLoginBtn = document.querySelector('.to-login');
-let toRegistrationBtn = document.querySelector('.to-registration');
-
-// back from registration/login  to start step
-let backToStartButtons = document.querySelectorAll('.enter-back-btn');
-// from 'in' to 'up' & vice versa
-let switchStepButtons = document.querySelectorAll('.switch-btn');
-
+// без реализации
 let submitSignUp = document.querySelector('.submit-sign-up');
-let submitSignIn = document.querySelector('.submit-sign-in');
 
 
 
-
-toLoginBtn.addEventListener('click', e => {
+//toLoginBtn
+document.querySelector('.to-login').addEventListener('click', e => {
 	startStep.toggleVisibility();
 	loginStep.toggleVisibility();
 });
-
-toRegistrationBtn.addEventListener('click', e => {
+//toRegistrationBtn
+document.querySelector('.to-registration').addEventListener('click', e => {
 	startStep.toggleVisibility();
 	registrationStep.toggleVisibility();
 });
 
+
+
+// back from registration/login  to start step buttons
 function backToStartStep() {
 	startStep.toggleVisibility();
 	registrationStep.hideVisibility();
 	loginStep.hideVisibility();
 }
-console.log(backToStartButtons);
+document.querySelectorAll('.enter-back-btn').forEach(backBtn => backBtn.addEventListener('click', backToStartStep));
 
 
-// пришлось ради двух элементов выносить функцию и делать for
-backToStartButtons.forEach(backBtn => backBtn.addEventListener('click', backToStartStep));
+
+// from 'in' to 'up' & vice versa
+let switchStepButtons = document.querySelectorAll('.switch-btn');
 
 switchStepButtons[0].addEventListener('click', e => {
 	e.preventDefault;
@@ -55,6 +51,13 @@ switchStepButtons[1].addEventListener('click', e => {
 	e.preventDefault;
 	registrationStep.toggleVisibility();
 	loginStep.toggleVisibility();
+});
+
+
+// sigh In teleport to main site
+document.querySelector('.submit-sign-in').addEventListener('click', e => {
+	e.preventDefault()
+	location.assign("http://127.0.0.1:5501/index.html")
 });
 
 
--- js/main.js
+++ js/main.js
@@ -1,63 +1,9 @@
 'use strict'
 
-let notificationsBell = document.querySelector('.notifications-btn');
-let notificationsX = document.querySelector('.close-bar__btn');
-let notificationsBar = document.querySelector('.notifications-bar');
-
-notificationsBell.addEventListener('click', e => {
-  let isNotifBarOn = notificationsBar.classList.toggle('notifications-bar__active')
-
-  if (isNotifBarOn) {
-    setTimeout(() => {
-      notificationsBar.classList.remove('notifications-bar__active')
-    }, 6000)
-  }
-})
-notificationsX.addEventListener('click', e => {
-  notificationsBar.classList.remove('notifications-bar__active')
-})
-
-////////////////////////////////
-document.querySelector('.left-panel__switcher').addEventListener('click', e => {
-  document.querySelector('.left-panel').classList.toggle('left-panel_active')
-})
-////////////////////////////
-
-
 /////////////////////////////////
 document.querySelector('.logout-btn').addEventListener('click', e => {
   location.assign("http://127.0.0.1:5501/enter-form.html")
 });
-
-document.querySelector('.submit-sign-in').addEventListener('click', e => {
-  e.preventDefault()
-  location.assign("http://127.0.0.1:5501/index.html")
-});
-/////////////////////////////////
-
-
-/////////////////////////
-let toBlockButtonsArray = document.querySelectorAll('.toblock-btn');;
-let blocksArray = document.querySelectorAll('.prim-blocks')
-let toBlockButtonsContainer = document.querySelector('.left-panel__toblock-links')
-
-toBlockButtonsContainer.addEventListener('click', function (e) {
-
-  toBlockButtonsArray.forEach((toBlockBtn) => {
-    toBlockBtn.classList.remove("toblock-btn_active");
-  });
-  blocksArray.forEach((block) => {
-    block.classList.remove("active-prim-block");
-  });
-
-  e.target.classList.add("toblock-btn_active");
-
-  //e.target.getAttribute('rel') - button number
-  blocksArray[e.target.getAttribute('rel')].classList.add("active-prim-block");
-
-  document.querySelector('.left-panel').classList.remove('left-panel_active')
-})
-
 
 
 ////////////////////////////
